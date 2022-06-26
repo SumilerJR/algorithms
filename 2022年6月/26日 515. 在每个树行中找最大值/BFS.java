a0cc7b927cdf28dc7baa575dc5043a76dc1a0410
SumilerJR <40729244+SumilerJR@users.noreply.github.com>
2022-06-26 11:22:47 +0800

    Create BFS.java

---
+++ 2022年6月/26日 515. 在每个树行中找最大值/BFS.java
@@ -0,0 +1,20 @@
+// - 广搜更简单就没写了，试试深搜理清层数关系也不难
+//执行用时：1 ms, 在所有 Java 提交中击败了93.73%的用户
+//内存消耗：41.4 MB, 在所有 Java 提交中击败了89.79%的用户
+class Solution {
+    List<Integer> list = new ArrayList<>();
+    public List<Integer> largestValues(TreeNode root) {
+        bfs(root, 0);
+        return list;
+    }
+    public void bfs(TreeNode root, int height) {
+        if (root == null)
+            return;
+        if (list.size() <= height)
+            list.add(root.val);
+        else if (list.get(height) < root.val)
+            list.set(height, root.val);
+        bfs(root.left, height + 1);
+        bfs(root.right, height + 1);
+    }
+}
